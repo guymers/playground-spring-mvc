9cddc05b62d3f30ececf0b00e6f7709df6a1ed5e
guymers <s@guymer.me>
2012-07-12 21:58:10 +1000

    update getAll to only select active widgets

--- src/main/java/me/guymer/spring/jpa/persistence/WidgetRepository.java
+++ src/main/java/me/guymer/spring/jpa/persistence/WidgetRepository.java
@@ -1,6 +1,14 @@
 package me.guymer.spring.jpa.persistence;
 
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 import me.guymer.spring.jpa.domain.Widget;
+import me.guymer.spring.jpa.domain.Widget_;
 
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +17,21 @@ public class WidgetRepository extends AbstractRepository<Widget> {
 	
 	public WidgetRepository() {
 		super(Widget.class);
+	}
+	
+	@Override
+	public List<Widget> getAll() {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Widget> criteriaQuery = criteriaBuilder.createQuery(Widget.class);
+		
+		Root<Widget> widget = criteriaQuery.from(Widget.class);
+		
+		criteriaQuery.select(widget);
+		criteriaQuery.where(criteriaBuilder.isTrue(widget.get(Widget_.active)));
+		
+		TypedQuery<Widget> query = entityManager.createQuery(criteriaQuery);
+		
+		return query.getResultList();
 	}
 	
 }
